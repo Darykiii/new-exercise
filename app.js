6b8bc1be3b4ba0b1c63ce4a15e84cdb57deefbd6
セックス <110929010+Darykiii@users.noreply.github.com>
2024-10-02 11:53:35 -0300

    att

--- app.js
+++ app.js
@@ -1,3 +1,22 @@
 alert("slv, cahorro!");
 let numerosecreto = 0;
- let chute = prompt("Escolha um número de 0 a 1000");
+let chute;
+let tentativas = 1;
+
+while ( chute!= numerosecreto){
+ chute = prompt("Escolha um número de -1000 a 1000");
+
+
+ 
+if(chute== numerosecreto){
+    console.log(`Isso aí!!! Você descobriu o número secreto!!! ${numerosecreto}`)
+}
+else{
+    if(numerosecreto>chute){
+    alert(`O número secreto é maior que ${chute}`)
+
+    }else{
+        alert(`O número secreto é menor que ${chute}`)
+    }
+}
+}
